e558b097db70156ce8d4d1ec02524aaf1910e929
Sodment <sodment@gmail.com>
2021-04-25 12:25:04 +0200

    Smarter way of passing lights to shaders

--- lights.h
+++ lights.h
@@ -1,4 +1,5 @@
 #pragma once
+#pragma warning(disable: 4996)
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm.hpp>
@@ -37,18 +38,66 @@ DirLight dirLight =
     glm::vec3(0.5f, 0.5f, 0.5f),
 };
 
+PointLight pointLights[2] =
+{
+    //1
+     glm::vec3(0.0f,  0.0f,  10.0f),
+    1.0f,
+    0.09,
+    0.032,
+    glm::vec3(0.05f, 0.05f, 0.05f),
+    glm::vec3(0.0f, 0.0f, 1.0f),
+    glm::vec3(1.0f, 1.0f, 1.0f),
+
+    //2
+    glm::vec3(0.0f,  0.0f,  -10.0f),
+    1.0f,
+    0.09,
+    0.032,
+    glm::vec3(0.05f, 0.05f, 0.05f),
+    glm::vec3(1.0f, 0.0f, 0.0f),
+    glm::vec3(1.0f, 1.0f, 1.0f),
+};
+
 void SetPointLight(ShaderProgram& sp, PointLight light, int index)
 {
-    char* string;
+    char string[26];
     sprintf(string, "pointLights[%d].position", index);
-    //glUniform3f(spNormalTexture->u("pointLights[0].position"), pointLightPositions[0][0], pointLightPositions[0][1], pointLightPositions[0][2]);
     glUniform3fv(glGetUniformLocation(sp.shaderProgram, string), 1, &light.position[0]);
-   // glUniform3f(spNormalTexture->u("pointLights[0].ambient"), 0.05f, 0.05f, 0.05f);
-    //glUniform3f(spNormalTexture->u("pointLights[0].diffuse"), 0.0f, 0.8f, 0.8f);
-   // glUniform3f(spNormalTexture->u("pointLights[0].specular"), 1.0f, 1.0f, 1.0f);
-   //glUniform1f(spNormalTexture->u("pointLights[0].constant"), 1.0f);
-   // glUniform1f(spNormalTexture->u("pointLights[0].linear"), 0.09);
-    //glUniform1f(spNormalTexture->u("pointLights[0].quadratic"), 0.032);
+    sprintf(string, "pointLights[%d].ambient", index);
+    glUniform3fv(glGetUniformLocation(sp.shaderProgram, string), 1, &light.ambient[0]);
+    sprintf(string, "pointLights[%d].diffuse", index);
+    glUniform3fv(glGetUniformLocation(sp.shaderProgram, string), 1, &light.diffuse[0]);
+    sprintf(string, "pointLights[%d].specular", index);
+    glUniform3fv(glGetUniformLocation(sp.shaderProgram, string), 1, &light.specular[0]);
+    sprintf(string, "pointLights[%d].constant", index);
+    glUniform1f(glGetUniformLocation(sp.shaderProgram, string), light.constant);
+    sprintf(string, "pointLights[%d].linear", index);
+    glUniform1f(glGetUniformLocation(sp.shaderProgram, string), light.linear);
+    sprintf(string, "pointLights[%d].quadratic", index);
+    glUniform1f(glGetUniformLocation(sp.shaderProgram, string), light.quadratic); 
+}
+
+void SetMulPointLight(ShaderProgram& sp, PointLight lights[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        char string[26];
+        sprintf(string, "pointLights[%d].position", i);
+        glUniform3fv(glGetUniformLocation(sp.shaderProgram, string), 1, &lights[i].position[0]);
+        sprintf(string, "pointLights[%d].ambient", i);
+        glUniform3fv(glGetUniformLocation(sp.shaderProgram, string), 1, &lights[i].ambient[0]);
+        sprintf(string, "pointLights[%d].diffuse", i);
+        glUniform3fv(glGetUniformLocation(sp.shaderProgram, string), 1, &lights[i].diffuse[0]);
+        sprintf(string, "pointLights[%d].specular", i);
+        glUniform3fv(glGetUniformLocation(sp.shaderProgram, string), 1, &lights[i].specular[0]);
+        sprintf(string, "pointLights[%d].constant", i);
+        glUniform1f(glGetUniformLocation(sp.shaderProgram, string), lights[i].constant);
+        sprintf(string, "pointLights[%d].linear", i);
+        glUniform1f(glGetUniformLocation(sp.shaderProgram, string), lights[i].linear);
+        sprintf(string, "pointLights[%d].quadratic", i);
+        glUniform1f(glGetUniformLocation(sp.shaderProgram, string), lights[i].quadratic);
+    }
 }
 
 void SetDirLight(ShaderProgram& sp, DirLight light)
