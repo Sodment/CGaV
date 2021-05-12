217e09969f3182f8751c0f92a93e3f79f3bc535d
Sodment <sodment@gmail.com>
2021-05-12 20:07:43 +0200

    Start work on Deferred shading in specular workflow

---
+++ g_buffer_specular.cpp
@@ -0,0 +1,42 @@
+#include "g_buffer_specular.h"
+
+GBufferSpecular::GBufferSpecular(unsigned int width, unsigned int height)
+{
+
+}
+
+void GBufferSpecular::ConfigureGBuffer()
+{
+    glGenFramebuffers(1, &gBuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, gBuffer);
+}
+
+void GBufferSpecular::SetupPositionColorBuffer(const int SCR_WIDTH, const int SCR_HEIGHT)
+{
+    glGenTextures(1, &gPosition);
+    glBindTexture(GL_TEXTURE_2D, gPosition);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGBA, GL_FLOAT, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, gPosition, 0);
+}
+
+void GBufferSpecular::SetupNormalColorBuffer(const int SCR_WIDTH, const int SCR_HEIGHT)
+{
+    glGenTextures(1, &gNormal);
+    glBindTexture(GL_TEXTURE_2D, gNormal);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGBA, GL_FLOAT, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, gNormal, 0);
+}
+
+void GBufferSpecular::SetupColorSpecularBuffer(const int SCR_WIDTH, const int SCR_HEIGHT)
+{
+    glGenTextures(1, &gAlbedoSpec);
+    glBindTexture(GL_TEXTURE_2D, gAlbedoSpec);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, GL_TEXTURE_2D, gAlbedoSpec, 0);
+}
---
+++ g_buffer_specular.h
@@ -0,0 +1,25 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm.hpp>
+#include <gtc/type_ptr.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include "shaderprogram.h"
+
+class GBufferSpecular
+{
+public:
+	GLuint gBuffer;
+	GLuint gPosition, gNormal, gAlbedoSpec;
+	GLuint rboDepth;
+	GBufferSpecular(unsigned int width, unsigned int height);
+private:
+	GLuint attachments[3] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2 };
+	void ConfigureGBuffer();
+	void SetupPositionColorBuffer(const int SCR_WIDTH, const int SCR_HEIGHT);
+	void SetupNormalColorBuffer(const int SCR_WIDTH, const int SCR_HEIGHT);
+	void SetupColorSpecularBuffer(const int SCR_WIDTH, const int SCR_HEIGHT);
+	void SetupDepthBuffer();
+};
